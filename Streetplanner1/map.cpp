3cac3e601347785b242db4e0cac7df4710baa9af
Huzy4401 <107009915+Huzy4401@users.noreply.github.com>
2022-06-07 02:35:01 +0200

    Update map.cpp

--- Streetplanner1/map.cpp
+++ Streetplanner1/map.cpp
@@ -7,6 +7,12 @@ void Map::addCity(City* city)
 {
     StaedteListe.push_back(*city);
     qDebug() << city->getName();
+    qDebug() << city;
+    for(auto it = StaedteListe.begin(); it != StaedteListe.end(); it++)
+    {
+        qDebug() << it->getName();
+        qDebug() << it;
+    }
 }
 
 void Map::draw(QGraphicsScene& scene)
@@ -26,15 +32,38 @@ void Map::draw(QGraphicsScene& scene)
 
 bool Map::addStreet(Street* street)
 {
-    /*int a = StaedteListe.indexOf(street->getObj1()->getName());
-    int b = StaedteListe.indexOf(street->getObj2()->getName());
-    if(a != -1 && b != -1)
-    {*/
+    bool a, b;
+    for(auto it = StaedteListe.begin(); it != StaedteListe.end(); it++)
+    {
+        if (it == street->getObj1())
+        {
+            a = true;
+        }
+        else
+        {
+            a = false;
+        }
+    }
+    for(auto it = StaedteListe.begin(); it != StaedteListe.end(); it++)
+    {
+        if (it == street->getObj2())
+        {
+            b = true;
+        }
+        else
+        {
+            b = false;
+        }
+    }
+    if(a && b )
+    {
         StrasseListe.push_back(*street);
+        qDebug() << "Success.";
         return true;
-    /*}
+    }
     else
     {
+        //qDebug() << "City doesnt exist.";
         return false;
-    }*/
+    }
 }
